24b6740a97368c813fb2ce9226b9e5494cafb7eb
Princewill Nwafor <nwaforprincewill21@gmail.com>
2025-01-30 03:18:58 +0100

    pls

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hp/pi',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5a10119deacbba6fe6fb72adb51d1809fdcb0c5c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hp/pi' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5a10119deacbba6fe6fb72adb51d1809fdcb0c5c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
